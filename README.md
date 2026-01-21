e3ade12afa350849d5e72898f48b33e23e4ba742
Chris Mumford <cmumford@cmumford.com>
2026-01-21 08:54:31 -0500

    Add first example program

---
+++ README.md
@@ -0,0 +1,10 @@
+A library for reading logs created by [RDS Spy](https://rdsspy.com/).
+
+There is a large collection of logs, from around the world, at https://github.com/walczakp/rds-spy-logs.
+This project references rds-spy-logs as a Git submodule and uses them in tests.
+
+# Example Program
+
+```sh
+cargo run --example sample third_party/rds-spy-logs/Austria/A540_-_2021-07-26_19-08-06.spy
+```
---
+++ example/sample.rs
@@ -0,0 +1,35 @@
+use rdspy::RdsGroupIterator;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let reader: Box<dyn BufRead> = if args.len() == 1 {
+        println!("Reading RDS groups from stdin...");
+        Box::new(BufReader::new(io::stdin()))
+    } else if args.len() == 2 {
+        let path = &args[1];
+        println!("Reading RDS groups from: {}", path);
+        Box::new(BufReader::new(File::open(path)?))
+    } else {
+        eprintln!("Usage: {} [path_to_rds_file]", args[0]);
+        eprintln!("(omit path to read from stdin)");
+        std::process::exit(1);
+    };
+
+    for group_result in RdsGroupIterator::new(reader) {
+        match group_result {
+            Ok(group) => {
+                println!(
+                    "A:{:04X} B:{:04X} C:{:04X} D:{:04X}",
+                    group.a, group.b, group.c, group.d
+                );
+            }
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+
+    Ok(())
+}
